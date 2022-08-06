48fbbf58203b0f1e68d4218d9a050edb80afd26c
Santiago Yate <santiyate7@gmail.com>
2022-08-05 22:35:56 -0500

    style: Unshaped landingpage structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>Hello world</h1>
+  <v-row>
+    <v-col cols="12">
+      <h1>Titulo</h1>
+    </v-col>
+    <v-col cols="12">
+      <h1>Busca y ofrece espacios de parqueo</h1>
+    </v-col>
+    <v-col cols="12">
+      <h1>Beneficios</h1>
+    </v-col>
+    <v-col cols="12">
+      <h1>Informacion corta y concisa</h1>
+    </v-col>
+    <v-col cols="12">
+      <h1>Informacion corta y concisa</h1>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
