8dba415c8227ee275cc5414323493e9c750e440d
Santiago Yate <santiyate7@gmail.com>
2022-08-06 10:14:19 -0500

    style: Space for images, created

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,37 @@
 <template>
   <v-row>
-    <v-col cols="12">
+    <!-- Titulo -->
+    <v-col cols="12" class="teal lighten-4 mt-1">
       <h1>Titulo</h1>
     </v-col>
-    <v-col cols="12">
+    <!-- Busca y ofrece espacios de parqueo -->
+    <v-col cols="12" class="teal lighten-4 mt-1">
       <h1>Busca y ofrece espacios de parqueo</h1>
       <Button @actionButton="actionButton" label="Buscar" />
     </v-col>
-    <v-col cols="12">
+    <!-- Beneficios -->
+    <v-col cols="12" class="teal lighten-4 mt-1">
       <h1>Beneficios</h1>
     </v-col>
-    <v-col cols="12">
-      <h1>Informacion corta y concisa</h1>
-      <Button @actionButton="actionButton" label="Buscar" />
+    <!-- Informacion corta y concisa -->
+    <v-col cols="12" class="teal lighten-4 mt-1">
+      <v-row>
+        <v-col cols="6" class="teal lighten-3"> Imagen </v-col>
+        <v-col cols="6">
+          <h1>Informacion corta y concisa</h1>
+          <Button @actionButton="actionButton" label="Buscar" />
+        </v-col>
+      </v-row>
     </v-col>
-    <v-col cols="12">
-      <h1>Informacion corta y concisa</h1>
-      <Button @actionButton="actionButton" label="Ofrecer" />
+    <!-- Informacion corta y concisa -->
+    <v-col cols="12" class="teal lighten-4 mt-1">
+      <v-row>
+        <v-col cols="6">
+          <h1>Informacion corta y concisa</h1>
+          <Button @actionButton="actionButton" label="Ofrecer" />
+        </v-col>
+        <v-col cols="6" class="teal lighten-3"> Imagen </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
