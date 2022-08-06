6e4125edc3d90ea2dbe4aa7052d73225e511cf19
Santiago Yate <santiyate7@gmail.com>
2022-08-05 22:58:15 -0500

    feat: Button component created

---
+++ components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-btn color="primary" @click="$emit('actionButton')" v-text="label" />
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "label",
+    },
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,15 +5,18 @@
     </v-col>
     <v-col cols="12">
       <h1>Busca y ofrece espacios de parqueo</h1>
+      <Button @actionButton="actionButton" label="Buscar" />
     </v-col>
     <v-col cols="12">
       <h1>Beneficios</h1>
     </v-col>
     <v-col cols="12">
       <h1>Informacion corta y concisa</h1>
+      <Button @actionButton="actionButton" label="Buscar" />
     </v-col>
     <v-col cols="12">
       <h1>Informacion corta y concisa</h1>
+      <Button @actionButton="actionButton" label="Ofrecer" />
     </v-col>
   </v-row>
 </template>
@@ -21,5 +24,10 @@
 <script>
 export default {
   name: "IndexPage",
+  methods: {
+    actionButton() {
+      $nuxt.$router.push({ name: "login" });
+    },
+  },
 };
 </script>
