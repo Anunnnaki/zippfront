7c4357ed5cfeedb9e9942369355f73a508275032
Santiago Yate <santiyate7@gmail.com>
2022-08-07 10:11:28 -0500

    style: Logic as soon as the green borders improved

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,38 @@
 <template>
-  <v-row class="primary">
+  <v-row class="white">
     <!-- Titulo -->
-    <v-col cols="12" class="white mt-1 px-10 py-6 d-flex">
+    <v-col cols="12" class="text-center pa-10 d-flex">
       <ZippLogo />
       <h1>Zipp</h1>
     </v-col>
     <!-- Busca y ofrece espacios de parqueo -->
-    <v-col cols="12" class="text-center white mt-1 pa-10">
+    <v-col
+      cols="12"
+      class="text-center pa-10"
+      style="border-top: 1px solid #bace00"
+    >
       <h1>Busca y ofrece espacios de parqueo</h1>
       <Button @actionButton="actionButton" label="Buscar" class="mt-4" />
     </v-col>
     <!-- Beneficios -->
-    <v-col cols="12" class="mt-1">
+    <v-col cols="12" class="text-center" style="border-top: 1px solid #bace00">
       <v-row>
-        <v-col class="white pa-10">
+        <v-col class="text-center pa-10">
           <h3>
             Encuentra espacios para todo tipo de vehiculos (carro, moto,
             bicicleta, patinenetas)
           </h3>
         </v-col>
-        <v-col class="mx-1 white pa-10">
+        <v-col
+          class="text-center pa-10"
+          style="
+            border-right: 1px solid #bace00;
+            border-left: 1px solid #bace00;
+          "
+        >
           <h3>Ofrece cualquier espacio disponible en tu casa u oficina</h3>
         </v-col>
-        <v-col class="white pa-10">
+        <v-col class="text-center pa-10">
           <h3>
             Rentabiliza tus zonas, tambien puedes darle opciones a tus clientes
             para que te visiten
@@ -31,10 +41,16 @@
       </v-row>
     </v-col>
     <!-- Informacion corta y concisa -->
-    <v-col cols="12" class="white mt-1">
+    <v-col cols="12" style="border-top: 1px solid #bace00">
       <v-row>
-        <v-col cols="6" class="grey"> Imagen </v-col>
         <v-col cols="6" class="text-center pa-10">
+          <NuxtLogo />
+        </v-col>
+        <v-col
+          cols="6"
+          class="text-center pa-10"
+          style="border-left: 1px solid #bace00"
+        >
           <h3>
             Puedes encontrar espacios para cualquier tipo de vehiculo, en
             cualquier lugar cerca o donde necesites ir
@@ -44,7 +60,7 @@
       </v-row>
     </v-col>
     <!-- Informacion corta y concisa -->
-    <v-col cols="12" class="white mt-1">
+    <v-col cols="12" style="border-top: 1px solid #bace00">
       <v-row>
         <v-col cols="6" class="text-center pa-10">
           <h3>
@@ -54,7 +70,13 @@
           </h3>
           <Button @actionButton="actionButton" label="Ofrecer" class="mt-4" />
         </v-col>
-        <v-col cols="6" class="grey"> Imagen </v-col>
+        <v-col
+          cols="6"
+          class="text-center pa-10"
+          style="border-left: 1px solid #bace00"
+        >
+          <NuxtLogo />
+        </v-col>
       </v-row>
     </v-col>
   </v-row>
