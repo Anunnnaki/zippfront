39d322507be3ed168c4bd61f039e10fac2bb7c3a
Santiago Yate <santiyate7@gmail.com>
2022-09-17 19:31:56 -0500

    feat: Side bar created, functional

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -1,47 +1,48 @@
 <template>
-  <v-navigation-drawer app permanent expand-on-hover>
+  <v-navigation-drawer app permanent expand-on-hover color="tertiary">
     <v-list>
       <v-list-item class="px-2">
         <v-list-item-avatar>
-          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+          <ZippLogo :width="40" :height="40" />
         </v-list-item-avatar>
-      </v-list-item>
-
-      <v-list-item link>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6"> Sandra Adams </v-list-item-title>
-          <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
-        </v-list-item-content>
+        <v-list-item-title>
+          <h1 class="white--text">Zipp</h1>
+        </v-list-item-title>
       </v-list-item>
     </v-list>
 
     <v-divider></v-divider>
 
     <v-list nav dense>
-      <v-list-item link>
+      <v-list-item
+        link
+        v-for="(item, i) in navigation.items"
+        :key="i"
+        :to="`/${item.to}`"
+        color="primary"
+      >
         <v-list-item-icon>
-          <v-icon>mdi-folder</v-icon>
+          <v-icon class="white--text">{{ `mdi-${item.icon}` }}</v-icon>
         </v-list-item-icon>
-        <v-list-item-title>My Files</v-list-item-title>
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon>mdi-account-multiple</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Shared with me</v-list-item-title>
-      </v-list-item>
-      <v-list-item link>
-        <v-list-item-icon>
-          <v-icon>mdi-star</v-icon>
-        </v-list-item-icon>
-        <v-list-item-title>Starred</v-list-item-title>
+        <v-list-item-title class="white--text">{{
+          item.title
+        }}</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
 </template>
 
 <script>
-export default {};
+import itemsNavigation from "~/config/itemsNavigation";
+export default {
+  data() {
+    return {
+      navigation: {
+        items: itemsNavigation,
+      },
+    };
+  },
+};
 </script>
 
 <style>
---
+++ config/itemsNavigation.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    icon: "map-marker",
+    title: "Zonas",
+    to: "zones",
+  },
+];
