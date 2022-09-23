006180e30fdefcb28338ec64863177bda2be4451
Santiago Yate <santiyate7@gmail.com>
2022-09-22 22:28:51 -0500

    feat: The map is integrated in home and the callsign icon is that of zipp

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,34 +5,29 @@ export default {
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - zipp_frontend',
-    title: 'zipp_frontend',
+    titleTemplate: "%s - zipp_frontend",
+    title: "zipp_frontend",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    '~/plugins/api'
-  ],
+  plugins: ["~/plugins/api"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -40,26 +35,28 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://www.npmjs.com/package/nuxt-vuex-localstorage
-    'nuxt-vuex-localstorage'
+    "nuxt-vuex-localstorage",
+    // https://www.npmjs.com/package/nuxt-leaflet
+    "nuxt-leaflet",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: "/",
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
       dark: false,
       themes: {
@@ -72,16 +69,15 @@ export default {
         //   error: colors.deepOrange.accent4,
         //   success: colors.green.accent3
         // },
-        light:{
-          primary: '#bace00',
-          secondary: '#1b676c', 
-          tertiary: '#575756',
-        }
-      }
-    }
+        light: {
+          primary: "#bace00",
+          secondary: "#1b676c",
+          tertiary: "#575756",
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
--- pages/home.vue
+++ pages/home.vue
@@ -1,9 +1,33 @@
 <template>
-  <h1>Home</h1>
+  <div id="map-wrap" style="height: 500px; width: 500px">
+    <client-only>
+      <l-map :zoom="13" :center="[5.063113580516123, -75.50212878432099]">
+        <l-tile-layer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        ></l-tile-layer>
+        <l-marker
+          :lat-lng="[5.063113580516123, -75.50212878432099]"
+          :icon="icon"
+        >
+        </l-marker>
+      </l-map>
+    </client-only>
+  </div>
 </template>
 
 <script>
-export default {};
+import L from "leaflet";
+export default {
+  data() {
+    return {
+      icon: L.icon({
+        iconUrl: "/img/zipp_logo.png",
+        iconSize: [50, 50],
+        iconAnchor: [16, 37],
+      }),
+    };
+  },
+};
 </script>
 
 <style>
